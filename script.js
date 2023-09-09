38022b6affc2ac9f9bd5af68e209e8d351137967
sreekarteegala <teegalasreekar@gmail.com>
2023-09-09 13:56:35 +0530

    Changes for responsive ness

--- script.js
+++ script.js
@@ -3,9 +3,17 @@
 let navItems = document.getElementsByTagName("navbar")[0].children;
 console.log(navItems)
 
-for(let item of navItems){
+for (let item of navItems) {
     item.addEventListener('click', (event) => {
         console.log(event.target.style)
         event.target.style.top = "20%";
     })
 }
+
+
+var menuButton = document.getElementById("menuButton");
+var menu = document.getElementById("menu");
+
+menuButton.addEventListener("click", function () {
+    menu.classList.toggle("open");
+});
