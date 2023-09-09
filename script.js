40da17ad2e88d2bcf23b9425fd6e1c4cb7d9a6ec
sreekarteegala <teegalasreekar@gmail.com>
2023-09-09 16:20:37 +0530

    Contact form using git actions

--- script.js
+++ script.js
@@ -18,8 +18,34 @@ menuButton.addEventListener("click", function () {
     menu.classList.toggle("open");
 });
 
-for(let item of menu.children){
+for (let item of menu.children) {
     item.addEventListener('click', (event) => {
         menu.classList.toggle("open");
     });
 }
+
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    fetch('https://github.com/sreekarteegala/portfolio-website/master', {
+        method: 'POST',
+        body: JSON.stringify(Object.fromEntries(formData)),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+        .then((response) => {
+            if (response.ok) {
+                alert('Form submitted successfully!');
+                e.target.reset();
+            } else {
+                alert('Form submission failed. Please try again.');
+            }
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            alert('An error occurred. Please try again later.');
+        });
+});
+
